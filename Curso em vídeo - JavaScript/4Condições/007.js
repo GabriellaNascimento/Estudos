c335c9545c452669bca9da7d344a760dbcc3f1ec
GabriellaNascimento <120525665+GabriellaNascimento@users.noreply.github.com>
2023-04-16 22:20:25 -0300

    pasta - condicionais

---
+++ Curso em vídeo - JavaScript/4Condições/007.js
@@ -0,0 +1,8 @@
+//Condição Simples
+
+var velocidade = 60 + 'km/h'
+console.log(`A velocidade do seu carro é ${velocidade}`)
+if (velocidade > 70 + 'km/h') {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ Curso em vídeo - JavaScript/4Condições/008.js
@@ -0,0 +1,7 @@
+var pais = 'Dinamarca'
+console.log(`Seu país é ${pais}.`)
+if (pais == 'Brasil' || pais == 'brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
