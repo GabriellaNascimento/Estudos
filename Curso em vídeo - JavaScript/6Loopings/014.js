4e8266aa0f3a1758afa2f0877cd0dd63cbd7a5ba
GabriellaNascimento <120525665+GabriellaNascimento@users.noreply.github.com>
2023-04-20 06:09:11 -0300

    pasta loopings - JS

---
+++ Curso em vídeo - JavaScript/6Loopings/014.js
@@ -0,0 +1,33 @@
+//While
+/*
+var contador = 1
+while (contador <= 5){
+    contador ++
+    console.log("Olá")
+}
+*/
+
+// Do While
+/*
+var c = 1
+do{
+    console.log("Tudo bem?")
+    c ++
+}while(c <= 6)
+*/
+
+// Diferença entre 'While' e 'Do While'
+/*
+ESSE GARANTE QUE A AÇÃO DENTRO DO BLOCO VAI SER EXECUTADA, PELO MENOS, UMA VEZ!
+var c = 1
+do{
+    console.log('Olá')
+}while(c < 0)
+*/
+
+/*
+var c = 1
+while(c < 0){
+    console.log('Olá')
+}
+*/
