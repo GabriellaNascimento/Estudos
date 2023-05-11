7fbbd3bc70fe12be838f967b0e8e6e890cf26e50
GabriellaNascimento <120525665+GabriellaNascimento@users.noreply.github.com>
2023-05-11 09:39:21 -0300

    criei a pasta matriz

---
+++ JavaScript/Matriz/matriz.js
@@ -0,0 +1,19 @@
+var mochila = [], item1= [], item2 = [];
+
+mochila.push(
+    item1 = ['Corda', 1],
+    item2 = ['Chave', 4]
+);
+
+console.log(mochila[1][1]);
+
+//Formas de usar matriz - arrays multidimensionais
+
+var mesa = [], comida, objetos;
+
+comida = ['Arroz', 4];
+objetos = ['Pratos', 5];
+
+mesa.push(comida, objetos);
+
+console.log(mesa[1][0]);
